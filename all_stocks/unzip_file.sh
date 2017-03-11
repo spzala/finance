e26c9bd314e18fb229e3f0a18a710d7662b565d6
Eduardo Patrocinio <epatro@gmail.com>
2017-03-11 09:59:40 -0500

    Download tickers

---
+++ all_stocks/unzip_file.sh
@@ -0,0 +1,15 @@
+ZIP_FILE="http://investexcel.net/wp-content/uploads/2015/01/Yahoo-Ticker-Symbols-Jan-2016.zip"
+OUTPUT_FILE="/tmp/tickers.zip"
+OUTPUT_DIR="/tmp/tickers"
+TICKER_FILE=tickers.xlsx
+
+wget $ZIP_FILE -O $OUTPUT_FILE
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
+rm *
+unzip -o $OUTPUT_FILE
+mv *.xlsx $TICKER_FILE
+
+echo File created in directory $OUTPUT_DIR
+
+
