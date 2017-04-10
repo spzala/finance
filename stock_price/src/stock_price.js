76fa357a33230726fd23c96f261eba5116a7d126
Eduardo Patrocinio <epatro@gmail.com>
2017-04-10 13:24:44 -0400

    Fix stock price microservice

--- stock_price/src/stock_price.js
+++ stock_price/src/stock_price.js
@@ -1,5 +1,5 @@
 var yahooFinance = require('yahoo-finance')
-
+var util = require('util')
 
 function retrieveStockPrice (stock, res) {
     console.log ("Querying stock: " + stock)
@@ -8,14 +8,14 @@ function retrieveStockPrice (stock, res) {
         symbol : stock,
         fields: ['a', 'b', 'b2', 'b3']
     }, function (err, snapshot) {
-        console.log ("Got response")
+        console.log ("Got response snapshot: " + util.inspect(snapshot))
         if (err) {
             console.log ("Error during retrieveStockPrice for stock: " + stock + ":" + err)
         }
         if (snapshot != undefined && snapshot.ask != null) {
             res.json ({symbol: snapshot.symbol, price: snapshot.ask})
         } else {
-            res.status(404).send ("Symbol not found")
+            res.status(404).send ("Symbol not found: " + stock)
         }
     })
 }
