cb3461171be6740a6c0a7221869450e1c2d09cae
Eduardo Patrocinio <epatro@gmail.com>
2017-04-19 17:56:12 -0400

    Integrate stock price with process stock

--- process_stock_symcol/src/process_stock.py
+++ process_stock_symcol/src/process_stock.py
@@ -3,15 +3,34 @@ from flask import Flask
 import json
 import pika
 import os
+import threading
+import requests
 
 app = Flask(__name__)
 
 channel_name = "stocks"
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+base_url = "http://stock_price:8080/stockPrice/GOOG"
 
-def consumeStocks():
+def callback(ch, method, properties, body):
+	resp = requests.get(base_url)
+	status = resp.status_code
+
+# check that we either got a successful response (200) or a previously retrieved, but still valid response (304)
+	if status == '200' or status == '304':
+		print 'Received response'
+#    reviews = toList(json.loads(resp[u'body'])[u'reviews'],u'reviewData')
+#    reviewerLists = map(uncompletedReviewers,reviews)
+#    reviewers = reduce(lambda a, b: set(a).union(set(b)), reviewerLists, set())
+#    print 'Incomplete Reviewers: '
+#    for r in reviewers:
+#        print '    ',r
+	else:
+		print 'Error status code: ', status
+
+	print(" [x] Received %r" % body)
+
+def consume():
 	print('Consuming stocks')
 	host = os.getenv('QUEUE_HOST', 'localhost')
 	print(host)
@@ -23,9 +42,13 @@ def consumeStocks():
 	channel.basic_consume(callback,
     	                  queue=channel_name,
         	              no_ack=True)
-
 	print(' [*] Waiting for messages. To exit press CTRL+C')
 	channel.start_consuming()
+
+
+def consumeStocks():
+	t = threading.Thread(target=consume)
+	t.start()
 
 @app.route("/process")
 def processStocks():
